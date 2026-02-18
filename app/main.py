759aa1d1f2c4e5f24671e1d1df925f266f732cc5
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-02-18 18:54:26 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import router  # Importa SOLO de routes/__init__.py
+from app.routes import router 
 
 app = FastAPI()
 
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(router)  # Incluye el router
+app.include_router(router)  
 
 @app.get("/")
 def read_root():
@@ -30,3 +30,4 @@ def read_root():
 @app.get("/health")
 async def health_check():
     return {"status": "healthy", "database_url": "configurada"}
+
