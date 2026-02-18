df72d81410fe1ecd489889ea1a732699b995075f
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-02-18 13:46:35 -0500

    Update db_config.py

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -2,6 +2,11 @@ import psycopg
 import os
 
 def get_db_connection():
-    return psycopg.connect(
-        os.getenv("DATABASE_URL")
-    )
+    database_url = os.getenv("DATABASE_URL")
+
+    if not database_url:
+        raise Exception("DATABASE_URL no configurada")
+
+    return psycopg.connect(database_url)
+
+
