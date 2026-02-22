19db254417bbd03147fd2d4080458be2ba70139e
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-02-22 15:08:02 -0500

    Update fotos_routes.py

--- app/routes/fotos_routes.py
+++ app/routes/fotos_routes.py
@@ -1,1 +1,13 @@
+from fastapi import APIRouter
+from app.config.db import get_connection
 
+router = APIRouter(prefix="/fotos", tags=["Fotos"])
+
+@router.get("/")
+def obtener_fotos():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM fotos_reporte")
+    data = cur.fetchall()
+    conn.close()
+    return data
