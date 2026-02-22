0d291a2c1e70b542df3bd31944b8cb5b7c490db5
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-02-22 15:08:24 -0500

    Update comentarios_routes.py

--- app/routes/comentarios_routes.py
+++ app/routes/comentarios_routes.py
@@ -1,1 +1,13 @@
+from fastapi import APIRouter
+from app.config.db import get_connection
 
+router = APIRouter(prefix="/comentarios", tags=["Comentarios"])
+
+@router.get("/")
+def obtener_comentarios():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM comentarios_reportes")
+    data = cur.fetchall()
+    conn.close()
+    return data
