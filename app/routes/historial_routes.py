0fff5ef4b4c95ca59d6caa0f25782816d911e726
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-02-22 15:08:38 -0500

    Update historial_routes.py

--- app/routes/historial_routes.py
+++ app/routes/historial_routes.py
@@ -1,1 +1,13 @@
+from fastapi import APIRouter
+from app.config.db import get_connection
 
+router = APIRouter(prefix="/historial", tags=["Historial"])
+
+@router.get("/")
+def obtener_historial():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM historial_reportes")
+    data = cur.fetchall()
+    conn.close()
+    return data
