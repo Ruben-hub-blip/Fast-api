c723c36f0cb095a84ab2cdef193cc8765b64fe96
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-02-22 15:07:46 -0500

    Update reportes_routes.py

--- app/routes/reportes_routes.py
+++ app/routes/reportes_routes.py
@@ -1,1 +1,13 @@
+from fastapi import APIRouter
+from app.config.db import get_connection
 
+router = APIRouter(prefix="/reportes", tags=["Reportes"])
+
+@router.get("/")
+def obtener_reportes():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM reportes")
+    data = cur.fetchall()
+    conn.close()
+    return data
