2b6ca88ec0e47f5929d3387ce2dfb8163f748709
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-02-22 15:07:31 -0500

    Update rol_modulo_routes.py

--- app/routes/rol_modulo_routes.py
+++ app/routes/rol_modulo_routes.py
@@ -1,1 +1,13 @@
+from fastapi import APIRouter
+from app.config.db import get_connection
 
+router = APIRouter(prefix="/rol-modulo", tags=["Rol_Modulo"])
+
+@router.get("/")
+def obtener_relaciones():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM rol_modulo")
+    data = cur.fetchall()
+    conn.close()
+    return data
