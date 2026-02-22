c7e545086d4a2a9edae4ba2985e75c8a6ba9d54a
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-02-22 15:07:11 -0500

    Update modulos_routes.py

--- app/routes/modulos_routes.py
+++ app/routes/modulos_routes.py
@@ -1,1 +1,13 @@
+from fastapi import APIRouter
+from app.config.db import get_connection
 
+router = APIRouter(prefix="/modulos", tags=["Modulos"])
+
+@router.get("/")
+def obtener_modulos():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM modulos")
+    data = cur.fetchall()
+    conn.close()
+    return data
