caa6bcaa1e89415ca8ad52b6288df9be7756f99a
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-02-22 15:06:48 -0500

    Update roles_routes.py

--- app/routes/roles_routes.py
+++ app/routes/roles_routes.py
@@ -1,1 +1,13 @@
+from fastapi import APIRouter
+from app.config.db import get_connection
 
+router = APIRouter(prefix="/roles", tags=["Roles"])
+
+@router.get("/")
+def obtener_roles():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM roles")
+    data = cur.fetchall()
+    conn.close()
+    return data
